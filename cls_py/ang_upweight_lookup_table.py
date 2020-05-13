ee6002584d287566a11c15f86169203f7c53f507
Srheft <s.eftekharzadeh@gmail.com>
2020-05-12 19:43:42 -0600

    update

--- cls_py/ang_upweight_lookup_table.py
+++ cls_py/ang_upweight_lookup_table.py
@@ -7,9 +7,7 @@ import os, sys, glob
 from astropy.table import Table,Column
 from time import time
 #### this directory the pairs of targets from 0 to 10 degress for the quasars in  NGC_QSO v7_2  at 0.8<z<2.2
-utahsyspath= 'for security reasons this is redacted in public codes'
-
-path = utahsyspath+'/sarahE/eboss/Apr2020/'
+path = '/uufs/chpc.utah.edu/common/home/astro/dawson/sarahE/eboss/Apr2020/'
 DDpath = path+'/DD_QSO_NGC_v7_2/'
 
 
@@ -17,17 +15,16 @@ max_ang = 1.0
 binsize=0.003
 
 start = time()
-#give_arr=True
-write_table = False # default
+give_arr=True
 
-#if give_arr: 
-if not os.path.exists(path+'NGC_QSO_v7_2_AngUpWeight_lookupTable_upto'+str(max_ang)+'_'+str(binsize)+'.fits'):    # True
-    
-    write_table = True
+if give_arr: #not os.path.exists(path+'/NGC_QSO_v7_2_pairs_stat_sep.fits'):    # True
 
-    cat = utahsyspath+'/sarahE/eboss/Mar2020/eBOSS_QSO_NGC_pip_v7_2.dat_z0.8_z2.2_withS_withPIX.fits'
+    cat = '/uufs/chpc.utah.edu/common/home/astro/dawson/sarahE/eboss/Mar2020/eBOSS_QSO_NGC_pip_v7_2.dat_z0.8_z2.2_withS_withPIX.fits'
     qso = fitsio.read(cat)
-
+    z = qso['Z']
+    ebid = qso['EBOSS_TARGET_ID']
+    ra = qso['RA']
+    dec = qso['DEC']
     status = qso['STATUS']
     files = glob.glob(DDpath+'*.fits')
 
@@ -35,14 +32,17 @@ if not os.path.exists(path+'NGC_QSO_v7_2_AngUpWeight_lookupTable_upto'+str(max_a
     ledges = np.round(np.arange(0,max_ang,binsize),3)
     hedges = np.round(np.arange(binsize,binsize+max_ang,binsize),3)
     mids = np.round((ledges+hedges)/2,3)
+    #print(list(zip(list(ledges),list(hedges),list(mids))))
     nbins = len(mids)
 
     dd_par = np.zeros(len(mids))
     dd_fb = np.zeros(len(mids))
-   
+    pip_fb = np.zeros(len(mids))
+
     for f in files:
     
         print('Working on ',f)
+        batch = f.split('spherematch.')[1].split('_')[0]
         a = fitsio.read(f)
         ind1 = a['index1']
         ind2 = a['index2']
@@ -50,24 +50,56 @@ if not os.path.exists(path+'NGC_QSO_v7_2_AngUpWeight_lookupTable_upto'+str(max_a
         stat1 = status[ind1]
         stat2 = status[ind2]
         angsep = a['dist']
+        pips = []
 
         for i in range(nbins):
-
+            
             wt = (ledges[i] < angsep) & (angsep <= hedges[i])
-
+            i1 = ind1[wt]
+            i2 = ind2[wt]
+            
             dd_par[i] += np.sum(wt)
     
-            # both members of the pair have to have gotten a fiber --> i.e., the 0 bit has to be flipped
-            w_fb = (((2**0 & stat1[wt]) !=0) & ((stat2[wt] & 2**0) !=0))
-          
-            dd_fb[i] += np.sum(w_fb)
+            w_fb = (((2**1 & stat1[wt]) !=0) & ((stat2[wt] & 2**1) !=0))
+            
+            if np.sum(w_fb) >0 :
 
-            print('Finished bin #{}:  {} < theta[deg] < {}: DD_pair/DD_fib = {}, total upweight for the bin so far: {}'.format(i,ledges[i],hedges[i],np.sum(wt)/np.sum(w_fb),dd_par[i]/dd_fb[i]))
+                ind1w = i1[w_fb]
+                ind2w = i2[w_fb]
+
+                for p in range(np.sum(w_fb)):
+
+                    c = (qso['WEIGHT_BW'][ind1w[p]] & qso['WEIGHT_BW'][ind2w[p]])
+                    cbit = [bin(ci) for ci in c]
+                    nones = sum([cb.count('1') for cb in cbit])
+
+                    if nones >0:
+                        pips.append(1860/nones)
+                    else:
+                        pips.append(0)
+
+            #### to get a taste of the measurement
+            #if i==0:
+                 
+            #     idradecz1= list(zip(i1,ra[i1],dec[i1],z[i1],status[i1]))
+            #     idradecz2= list(zip(i2,ra[i2],dec[i2],z[i2],status[i2]))
+
+            #     for i in range(11):
+
+            #          print(idradecz1[i],'\t', idradecz2[i],'\t','{:>8}'.format(angsep[i]),'\t','{:>8}'.format(np.round(pips[i],4)))
+
+ 
+            dd_fb[i] += np.sum(w_fb)
+            pip_fb[i] += np.sum(pips) 
+            print('Finished bin #{} in batch {}:  {} < theta[deg] < {}: DD_pair/DD_fib = {}, total upweight for the bin so far: {},{}'.format(i,batch,ledges[i],hedges[i],np.sum(wt)/np.sum(w_fb),dd_par[i]/dd_fb[i], dd_par[i]/pip_fb[i], dd_par[i]*pip_fb[i]))
        
+       # sephist,b = np.histogram(angsep,bins = [0,0.1,.3,.4,.5])
+       # print(sephist,b)
    
 angup = dd_par/dd_fb      
+angupw = dd_par/dd_fb/pips
 
-
+write_table = True
 
 if write_table:
 
@@ -88,10 +120,14 @@ if write_table:
     cc = Column(angup,name='AngUpWeight')
     a.add_column(cc,index=0)
 
+    cc = Column(angupw,name='AngUpWeight_wpip')
+    a.add_column(cc,index=0)
+
+    cc = Column(pips,name='pipsforDD_fib')
+    a.add_column(cc,index=0)
+
     a.write(path+'NGC_QSO_v7_2_AngUpWeight_lookupTable_upto'+str(max_ang)+'_'+str(binsize)+'.fits')
     
-    print('Finished writing the lookup Table of the angular upweights! took {} min'.format((time()-start)/60.)) 
-
- else:
-    print('The requested look up table already exist here: {}'.format(path+'NGC_QSO_v7_2_AngUpWeight_lookupTable_upto'+str(max_ang)+'_'+str(binsize)+'.fits'))
+print('Finished writing the lookup Table of the angular upweights! took {} min'.format((time()-start)/60.)) 
+    
    
